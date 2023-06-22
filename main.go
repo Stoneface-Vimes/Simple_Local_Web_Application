f007f7eff9fad1b7703fdbebbb24736796868405
Cliff Weir <cliff.weir@kindred.ai>
2023-06-22 01:04:00 -0400

    Section 3, Part 24:  Error Checking

--- main.go
+++ main.go
@@ -8,6 +8,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -42,6 +43,25 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by zero")
+		return
+	}
+
+	fmt.Fprintf(w, "%f divided by %f is %f", 100.0, 0.0, f)
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // main is the main application function
 func main() {
 
@@ -49,6 +69,7 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	//Prints the port in use by the application to the terminal
 	fmt.Printf("Starting application on port %s", portNumber)
