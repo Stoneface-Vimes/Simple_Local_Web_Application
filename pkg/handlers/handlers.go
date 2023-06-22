5a2ae890e6350dd23efdcda0bad4a76a77982596
Cliff Weir <cliff.weir@kindred.ai>
2023-06-22 02:39:50 -0400

    RenderTemplate is now referenced correctly

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"Simple_Local_Web_Application/pkg/render"
 	"net/http"
 )
 
@@ -9,10 +10,10 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,10 +6,9 @@ import (
 	"text/template"
 )
 
-//The renderTemplate function is used to render templates on our various pages. Requires a response (w) and the
-//type of template that is being rendered (html, tmpl, etc)
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
+// The renderTemplate function is used to render templates on our various pages. Requires a response (w) and the
+// type of template that is being rendered (html, tmpl, etc)
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	//ParsedTemplate is used to store the template that will be rendered. The template that is stored
 	//will depend on the string provided when renderTemplate is called
