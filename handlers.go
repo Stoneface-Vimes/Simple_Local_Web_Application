fdc15d5540779a661b12ad673a6b58593fe29434
Cliff Weir <cliff.weir@kindred.ai>
2023-06-22 02:07:41 -0400

    Part 27, moved render function to render.go

--- handlers.go
+++ handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // In order for a function to respond to a request from a web browser, it has to handle two parameters:
@@ -17,19 +15,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.page.tmpl")
-}
-
-//The renderTemplate function is used to render templates on our various pages. Requires a response (w) and the
-//type of template that is being rendered (html, tmpl, etc)
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-
-	//ParsedTemplate is used to store the template that will be rendered. The template that is stored
-	//will depend on the string provided when renderTemplate is called
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template", err)
-		return
-	}
 }
---
+++ render.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+//The renderTemplate function is used to render templates on our various pages. Requires a response (w) and the
+//type of template that is being rendered (html, tmpl, etc)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+
+	//ParsedTemplate is used to store the template that will be rendered. The template that is stored
+	//will depend on the string provided when renderTemplate is called
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template", err)
+		return
+	}
+}
