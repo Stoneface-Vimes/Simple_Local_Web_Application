a87cc999dcdac450157d16e6be790d7378acbe3b
Cliff Weir <cliff.weir@kindred.ai>
2023-06-28 00:44:48 -0400

    Added base layout to the parsedTemplate function

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -12,7 +12,10 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	//ParsedTemplate is used to store the template that will be rendered. The template that is stored
 	//will depend on the string provided when renderTemplate is called
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	//Added the base layout tmpl file to the files being parsed
+	//Currently, every time you visit a page the template must be read from disk. In the future,
+	//the parsed templates will be saved to a local variable
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template", err)
